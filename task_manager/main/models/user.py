6a36174085d0b3d711a96bfff01e77e44d0b466e
Roman Baksheev <rombaks.dev@gmail.com>
2022-10-06 00:28:31 +0500

    Update user model in main app

--- task_manager/main/models/user.py
+++ task_manager/main/models/user.py
@@ -1,5 +1,25 @@
+from tabnanny import verbose
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass 
+    first_name = models.CharField(max_length=255, verbose_name="First name")
+    last_name = models.CharField(max_length=255, verbose_name="Last name")
+    email = models.EmailField(max_length = 254, verbose_name="E-mail")
+
+    class Roles(models.TextChoices):
+        DEVELOPER = "developer"
+        MANAGER = "manager"
+        ADMIN = "admin"
+
+    role = models.CharField(max_length=255, verbose_name="Role",
+        default=Roles.DEVELOPER, choices=Roles.choices)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        ordering = ['last_name']
