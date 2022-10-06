2bde6693cafb23063372a1fc9accf877ab4005ca
Roman Baksheev <rombaks.dev@gmail.com>
2022-10-06 12:00:35 +0500

    Add user model in main app

--- task_manager/main/models/user.py
+++ task_manager/main/models/user.py
@@ -6,20 +6,24 @@ from django.db import models
 class User(AbstractUser):
     first_name = models.CharField(max_length=255, verbose_name="First name")
     last_name = models.CharField(max_length=255, verbose_name="Last name")
-    email = models.EmailField(max_length = 254, verbose_name="E-mail")
+    email = models.EmailField(max_length=254, verbose_name="E-mail")
 
     class Roles(models.TextChoices):
         DEVELOPER = "developer"
         MANAGER = "manager"
         ADMIN = "admin"
 
-    role = models.CharField(max_length=255, verbose_name="Role",
-        default=Roles.DEVELOPER, choices=Roles.choices)
+    role = models.CharField(
+        max_length=255,
+        verbose_name="Role",
+        default=Roles.DEVELOPER,
+        choices=Roles.choices,
+    )
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = 'User'
-        verbose_name_plural = 'Users'
-        ordering = ['last_name']
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+        ordering = ["last_name"]
