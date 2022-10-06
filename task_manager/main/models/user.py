4e2b5f74f9dcd285794558f6c7f2eac4001d3449
Roman Baksheev <rombaks.dev@gmail.com>
2022-10-07 01:00:14 +0500

    Make migrations for custom user model

--- task_manager/main/models/user.py
+++ task_manager/main/models/user.py
@@ -1,17 +1,59 @@
 from tabnanny import verbose
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
 
-class User(AbstractUser):
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=255, verbose_name="First name")
     last_name = models.CharField(max_length=255, verbose_name="Last name")
-    email = models.EmailField(max_length=254, verbose_name="E-mail")
+    email = models.EmailField(max_length=254, verbose_name="E-mail", unique=True)
+    
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ()
 
     class Roles(models.TextChoices):
-        DEVELOPER = "developer"
-        MANAGER = "manager"
-        ADMIN = "admin"
+        DEVELOPER = "Developer"
+        MANAGER = "Manager"
+        ADMIN = "Admin"
 
     role = models.CharField(
         max_length=255,
@@ -20,10 +62,14 @@ class User(AbstractUser):
         choices=Roles.choices,
     )
 
+    objects = CustomUserManager()
+    
+
     def __str__(self):
-        return self.title
+        return f'{self.first_name[:1]}.{self.last_name}.{self.role[:3]}.id{self.id}'
 
     class Meta:
         verbose_name = "User"
         verbose_name_plural = "Users"
-        ordering = ["last_name"]
+
+
