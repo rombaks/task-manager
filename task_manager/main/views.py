173cc4300620a0fca58ec74309065612bab5bece
Roman Baksheev <rombaks.dev@gmail.com>
2022-10-13 12:33:58 +0400

    Update user field in views.py

--- task_manager/main/views.py
+++ task_manager/main/views.py
@@ -10,7 +10,7 @@ class UserFilter(django_filters.FilterSet):
 
     class Meta:
         model = User
-        fields = ("name",)
+        fields = ("first_name",)
 
 
 class UserViewSet(viewsets.ModelViewSet):
