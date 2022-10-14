52d48b1237b9cf668719ff47f2e1804bc9917f8e
Roman Baksheev <rombaks.dev@gmail.com>
2022-10-14 18:14:24 +0400

    Add rest_framework, django_filters and markdown in settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -39,7 +39,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    "rest_framework",
+    "django_filters",
+    "markdown",
     "drf_yasg",
+
     "task_manager.main.apps.MainConfig",
 ]
 
