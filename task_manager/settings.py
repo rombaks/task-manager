0be029d2d36833eb5cd4b3d3506fac3eaaa4bdfd
Roman Baksheev <rombaks.dev@gmail.com>
2022-10-15 23:33:26 +0400

    Add TEST_REQUEST_DEFAULT_FORMAT json in settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -65,6 +65,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+    "TEST_REQUEST_DEFAULT_FORMAT": "json",
 }
 
 TEMPLATES = [
