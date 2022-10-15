4fb3b43af4837e402d4efc5874c617c342eb2b80
Roman Baksheev <rombaks.dev@gmail.com>
2022-10-15 23:28:05 +0400

    Add test_create in test_tag.py

--- tests/test_tag.py
+++ tests/test_tag.py
@@ -9,11 +9,7 @@ class TestTagViewSet(TestViewSetBase):
     basename = "tags"
     tag_attributes = factory.build(dict, FACTORY_CLASS=TagFactory)
 
-    @staticmethod
-    def expected_details(entity: dict, attributes: dict):
-        return {**attributes, "id": entity["id"]}
-
-    # def test_create(self):
-    #     tag = self.create(self.tag_attributes)
-    #     expected_response = self.expected_details(tag, self.tag_attributes)
-    #     assert tag == expected_response
+    def test_create(self):
+        tag = self.create(self.tag_attributes)
+        expected_response = self.tag_attributes
+        assert tag == expected_response
