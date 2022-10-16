f0e5fc17b60ec8e9b125deeb3e43674ee97f6150
Roman Baksheev <rombaks.dev@gmail.com>
2022-10-16 10:44:56 +0400

    Add djangorestframework-simplejwt with poetry

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -19,6 +19,10 @@ from django.urls import include, path, re_path
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions, routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from .main.views import UserViewSet, TaskViewSet, TagViewSet
 
@@ -44,6 +48,8 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     re_path(
         r"^swagger(?P<format>\.json|\.yaml)$",
         schema_view.without_ui(cache_timeout=0),
