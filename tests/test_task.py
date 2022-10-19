e361dd222f6bebf0c2c6e71e88881111a22eaf37
Roman Baksheev <rombaks.dev@gmail.com>
2022-10-19 22:43:32 +0400

    Add test_list in test_task.py

--- tests/test_task.py
+++ tests/test_task.py
@@ -8,10 +8,21 @@ from factories import TaskFactory
 class TestTaskViewSet(TestViewSetBase):
     basename = "tasks"
     task_attributes = factory.build(dict, FACTORY_CLASS=TaskFactory)
+    BATCH_SIZE = 3
+    tasks_attributes = factory.build_batch(
+        dict, FACTORY_CLASS=TaskFactory, size=BATCH_SIZE
+    )
 
     @staticmethod
     def expected_details(entity: dict, attributes: dict):
         return {**attributes, "id": entity["id"]}
+
+    def expected_list(self, entity_list: list[dict], attributes_list: list[dict]):
+        expected_list = []
+        for index, entity in enumerate(entity_list):
+            entity_details = self.expected_details(entity, attributes_list[index])
+            expected_list.append(entity_details)
+        return expected_list
 
     def test_create(self):
         task = self.create(self.task_attributes)
@@ -23,6 +34,12 @@ class TestTaskViewSet(TestViewSetBase):
         id = self.expected_details(task, self.task_attributes)["id"]
         expected_response = self.retrieve(id=id)
         assert task == expected_response
+
+    def test_list(self):
+        tasks = self.create_batch(self.tasks_attributes)
+        expected_response = self.expected_list(tasks, self.tasks_attributes)
+        task_list = self.list()
+        assert task_list == expected_response
 
     def test_update(self):
         task = self.create(self.task_attributes)
