8ff22e22083e37b7da7e401f237249fced7618f3
Roman Baksheev <rombaks.dev@gmail.com>
2022-10-19 23:45:11 +0400

    Add test_list in test_tag.py

--- tests/test_tag.py
+++ tests/test_tag.py
@@ -1,5 +1,6 @@
 import factory
 from http import HTTPStatus
+import json
 
 from base_test_views import TestViewSetBase
 from factories import TagFactory
@@ -9,9 +10,21 @@ class TestTagViewSet(TestViewSetBase):
     basename = "tags"
     tag_attributes = factory.build(dict, FACTORY_CLASS=TagFactory)
 
+    BATCH_SIZE = 3
+    tags_attributes = factory.build_batch(
+        dict, FACTORY_CLASS=TagFactory, size=BATCH_SIZE
+    )
+
     @staticmethod
     def expected_details(entity: dict, attributes: dict):
         return {**attributes, "id": entity["id"]}
+
+    def expected_list(self, entity_list: list[dict], attributes_list: list[dict]):
+        expected_list = []
+        for index, entity in enumerate(entity_list):
+            entity_details = self.expected_details(entity, attributes_list[index])
+            expected_list.append(entity_details)
+        return expected_list
 
     def test_create(self):
         tag = self.create(self.tag_attributes)
@@ -23,6 +36,12 @@ class TestTagViewSet(TestViewSetBase):
         id = self.expected_details(tag, self.tag_attributes)["id"]
         expected_response = self.retrieve(id=id)
         assert tag == expected_response
+
+    def test_list(self):
+        tags = self.create_batch(self.tags_attributes)
+        expected_response = self.expected_list(tags, self.tags_attributes)
+        tag_list = self.list()
+        assert tag_list == expected_response
 
     def test_update(self):
         tag = self.create(self.tag_attributes)
@@ -43,5 +62,6 @@ class TestTagViewSet(TestViewSetBase):
         assert expected_response.status_code == HTTPStatus.NOT_FOUND
 
     def test_anonymous_create(self):
-        expected_response = self.anonymous_create(self.tag_attributes)
-        assert expected_response.status_code == HTTPStatus.FORBIDDEN
+        response = self.anonymous_create(self.tag_attributes)
+        expected_error = "Authentication credentials were not provided."
+        assert response["detail"] == expected_error
