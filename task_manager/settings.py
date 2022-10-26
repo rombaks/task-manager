4488247efba82e1cee113c8658b105247efb6a69
Roman Baksheev <rombaks.dev@gmail.com>
2022-10-26 23:28:51 +0400

    Set ALLOWED_HOSTS

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "my_default_secret_key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
 
 
 # Application definition
@@ -149,8 +149,8 @@ STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"\
+
 # Configure Django App for Heroku.
 import django_on_heroku
-
 django_on_heroku.settings(locals())
