7fe691fdfee1b37abbce342f2197e91a6bbb18ba
Roman Baksheev <rombaks.dev@gmail.com>
2022-10-27 00:06:13 +0400

    Add error for rollback

--- task_manager/main/views.py
+++ task_manager/main/views.py
@@ -1,3 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
 from rest_framework import viewsets
 import django_filters
 
@@ -5,6 +8,12 @@ import django_filters
 from .models import User, Task, Tag
 from .serializers import UserSerializer, TaskSerializer, TagSerializer
 from .filters import UserFilter, TaskFilter
+
+
+def index(request):
+    a = None
+    a.hello() # Creating an error with an invalid line of code
+    return HttpResponse("Hello, world. You're at the pollapp index.")
 
 
 class UserViewSet(viewsets.ModelViewSet):
