179f2769ae03de89fef2b9c3ef9e33b53584d4c0
rombaks <104414410+rombaks@users.noreply.github.com>
2022-11-06 11:33:27 +0400

    Update settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
 ]
 
 ROLLBAR = {
-    "access_token": "e85f8bf72fc246d88d6a842ce039bb46",
+    "access_token": os.environ["POST_SERVER_ITEM_ACCESS_TOKEN"],
     "environment": "development" if DEBUG else "production",
     "code_version": "1.0",
     "root": BASE_DIR,
