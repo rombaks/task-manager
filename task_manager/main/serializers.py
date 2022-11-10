8f2829aec02cd2b09bb92dedd87b720a748136b1
Roman Baksheev <rombaks.dev@gmail.com>
2022-11-10 04:14:07 +0400

    Set file max size validators in serizlizers.py

--- task_manager/main/serializers.py
+++ task_manager/main/serializers.py
@@ -1,8 +1,28 @@
 from rest_framework import serializers
+from django.core.files.base import File
+from django.core.validators import FileExtensionValidator
+
 from .models import tag, user, task
+from task_manager.settings import UPLOAD_MAX_SIZES
+
+class FileMaxSizeValidator:
+    def __init__(self, max_size: int) -> None:
+        self.max_size = max_size
+
+    def __call__(self, value: File) -> None:
+        if value.size > self.max_size:
+            raise ValidationError(f"Maximum size {self.max_size} exceeded.")
 
 
 class UserSerializer(serializers.ModelSerializer):
+    avatar_picture = serializers.FileField(
+        required=False,
+        validators=[
+            FileMaxSizeValidator(UPLOAD_MAX_SIZES["avatar_picture"]),
+            FileExtensionValidator(["jpeg", "jpg", "png"]),
+        ],
+    )
+
     class Meta:
         model = user.User
         fields = (
