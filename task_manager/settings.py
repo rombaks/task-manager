4de77a8209adbbc336a69c74cf47ad07fa1613f6
Roman Baksheev <rombaks.dev@gmail.com>
2022-11-12 23:13:06 +0500

    Set TEST_REQUEST_DEFAULT_FORMAT to default

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -74,7 +74,6 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
-    "TEST_REQUEST_DEFAULT_FORMAT": "json",
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
