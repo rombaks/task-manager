c73d2b8ce93a6634fc03b315b0b23eeb26eb8922
Roman Baksheev <rombaks.dev@gmail.com>
2023-02-17 20:57:30 +0400

    Add ActionClient in base_test_views.py

--- tests/base_test_views.py
+++ tests/base_test_views.py
@@ -1,18 +1,20 @@
 import factory
 
 from http import HTTPStatus
-from typing import List, Union
+from typing import List, Optional, Union
 
 from django.urls import reverse
 from rest_framework.test import APIClient, APITestCase
 
 from task_manager.main.models import User
 from fixtures.factories import UserFactory
+from tests.fixtures.action_client import ActionClient
 
 
 class TestViewSetBase(APITestCase):
     user: User = None
     client: APIClient = None
+    action_client: Optional[ActionClient] = None
     basename: str
 
     @classmethod
@@ -20,6 +22,7 @@ class TestViewSetBase(APITestCase):
         super().setUpTestData()
         cls.user = cls.create_api_user()
         cls.client = APIClient()
+        cls.action_client = ActionClient(cls.client)
 
     @staticmethod
     def create_api_user():
@@ -35,8 +38,7 @@ class TestViewSetBase(APITestCase):
         return reverse(f"{cls.basename}-list", args=args)
 
     def create(self, data: dict, args: List[Union[str, int]] = None) -> dict:
-        self.client.force_login(self.user)
-        response = self.client.post(self.list_url(args), data=data)
+        response = self.request_create(data, args)
         assert response.status_code == HTTPStatus.CREATED, response.content
         return response.data
 
