9520b0bbb87ce9d8ca9c0207d93c1c0860eb1353
Roman Baksheev <rombaks.dev@gmail.com>
2023-02-17 17:30:20 +0400

    Add test_patch in test_current_user.py

--- tests/test_current_user.py
+++ tests/test_current_user.py
@@ -11,3 +11,9 @@ class TestUserViewSet(TestViewSetBase):
     def test_retrieve(self):
         user = self.single_resource()
         assert user.get("id") == self.user.id
+
+    def test_patch(self):
+        self.patch_single_resource({"first_name": "TestName"})
+
+        user = self.single_resource()
+        assert user["first_name"] == "TestName"
