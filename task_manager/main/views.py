40d6912129ebd26032374eac144d854365ce59e4
Roman Baksheev <rombaks.dev@gmail.com>
2023-03-01 12:57:09 +0400

    Add AsyncJobViewSet in views.py

--- task_manager/main/views.py
+++ task_manager/main/views.py
@@ -1,16 +1,22 @@
-from typing import cast
+from http import HTTPStatus
+from typing import Any, cast
 
+from django.http import Http404, HttpResponse
 from django.urls import reverse
 from rest_framework import mixins, viewsets
+from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework_extensions.mixins import NestedViewSetMixin
+from task_manager.main.services.async_celery import AsyncJob, JobStatus
 
 from .filters import TaskFilter, UserFilter
 from .models import Tag, Task, User
 from .serializers import (
+    CountdownJobSerializer,
     TagSerializer,
     TaskSerializer,
     UserSerializer,
-    CountdownJobSerializer,
+    JobSerializer,
 )
 from .services.nested_resource import TaskTagsViewMixin
 from .services.single_resource import SingleResourceMixin, SingleResourceUpdateMixin
@@ -66,3 +72,27 @@ class CountdownJobViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     def get_success_headers(self, data: dict) -> dict[str, str]:
         task_id = data["task_id"]
         return {"Location": reverse("jobs-detail", args=[task_id])}
+
+
+class AsyncJobViewSet(viewsets.GenericViewSet):
+    serializer_class = JobSerializer
+
+    def get_object(self) -> AsyncJob:
+        lookup_url_kwargs = self.lookup_url_kwarg or self.lookup_field
+        task_id = self.kwargs[lookup_url_kwargs]
+        job = AsyncJob.from_id(task_id)
+        if job.status == JobStatus.UNKNOWN:
+            raise Http404()
+        return job
+
+    def retrieve(self, request: Request, *args: Any, **kwargs: Any) -> HttpResponse:
+        instance = self.get_object()
+        serializer_data = self.get_serializer(instance).data
+        if instance.status == JobStatus.SUCCESS:
+            location = self.request.build_absolute_uri(instance.result)
+            return Response(
+                serializer_data,
+                headers={"location": location},
+                status=HTTPStatus.CREATED,
+            )
+        return Response(serializer_data)
